9104f4f87a66893f272349a92ac82f311832d1c9
LeonardoFernandes <leonardo.ff.21@gmail.com>
2026-03-03 01:28:46 -0300

    feat: add shimmer skeleton loading and filter out producers with invalid images

--- frontend/src/components/Carousel/index.tsx
+++ frontend/src/components/Carousel/index.tsx
@@ -2,35 +2,53 @@ import { useEffect, useState } from "react";
 import { getProducers } from "../../services/producersService";
 import type { Producer } from "../../types/producer";
 import { Card } from "../Card";
+import { SkeletonCard } from "../SkeletonCard";
 
 export const Carousel = () => {
   const [producers, setProducers] = useState<Producer[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const validateImage = (url: string): Promise<boolean> => {
+    return new Promise((resolve) => {
+      const img = new Image();
+      img.src = url;
+
+      img.onload = () => resolve(true);
+      img.onerror = () => resolve(false);
+    });
+  };
 
   useEffect(() => {
     const fetchProducers = async () => {
       const data = await getProducers();
-      setProducers(data);
+
+      const validated = await Promise.all(
+        data.map(async (producer) => {
+          const isValid = await validateImage(producer.imageUrl);
+          return isValid ? producer : null;
+        }),
+      );
+
+      const filtered = validated.filter(Boolean) as Producer[];
+
+      setProducers(filtered);
+      setLoading(false);
     };
 
     fetchProducers();
   }, []);
 
-  if (!producers.length) {
-    return (
-      <div className="flex justify-center items-center h-64">
-        <p className="text-gray-500">Loading...</p>
-      </div>
-    );
-  }
-
-  const infiniteList = [...producers, ...producers];
+  // Quantidade de skeletons visíveis
+  const skeletons = Array.from({ length: 6 });
 
   return (
     <section className="w-screen overflow-hidden">
-      <div className="flex w-max animate-marquee ">
-        {infiniteList.map((producer, index) => (
-          <Card key={index} producer={producer} />
-        ))}
+      <div className="flex w-max animate-marquee">
+        {loading
+          ? skeletons.map((_, index) => <SkeletonCard key={index} />)
+          : [...producers, ...producers].map((producer, index) => (
+              <Card key={index} producer={producer} />
+            ))}
       </div>
     </section>
   );
---
+++ frontend/src/components/SkeletonCard/index.tsx
@@ -0,0 +1,5 @@
+export const SkeletonCard = () => {
+  return (
+    <div className="relative flex-shrink-0 w-72 aspect-[3/4] mr-6 rounded-3xl overflow-hidden bg-gray-300 skeleton-shimmer"></div>
+  );
+};
